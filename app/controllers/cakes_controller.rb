289037b2d5c29dcfe5b4e29cbe7320b2ff643ce3
Bernard Teles <bernardtelgon@gmail.com>
2024-04-10 20:55:45 -0300

    finish study

--- app/controllers/cakes_controller.rb
+++ app/controllers/cakes_controller.rb
@@ -2,6 +2,51 @@ class CakesController < ApplicationController
 
   def index
     @cakes = Cake.all
+
+  end
+
+  def show
+    @cake = Cake.find(params[:id])
+  end
+
+  def new
+    @cake = Cake.new
+
+  end
+
+  def create
+    @cake = Cake.new(cake_params)
+    @cake.save
+    redirect_to cake_path(@cake)
+
+  end
+
+  def edit
+    @cake = Cake.find(params[:id])
+
+  end
+
+  def update
+    @cake = Cake.find(params[:id])
+    @cake.update(cake_params)
+    redirect_to cake_path(@cake)
+
+  end
+
+  def destroy
+    @cake = Cake.find(params[:id])
+    @cake.destroy
+    redirect_to cakes_path
     
   end
+
+
+  private
+
+  def cake_params
+    params.require(:cake).permit(:name, :address, :stars)
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,16 @@ Rails.application.routes.draw do
 #listar todos os cakes
 get 'cakes', to: 'cakes#index'
 
+get 'cakes/new', to: 'cakes#new'
+
+get 'cakes/:id', to: 'cakes#show', as: 'cake'
+
+post 'cakes', to: 'cakes#create'
+
+get 'cakes/:id/edit', to: 'cakes#edit', as: 'edit_cake'
+
+patch 'cakes/:id', to: 'cakes#update'
+
+delete 'cakes/:id', to: 'cakes#destroy'
+
 end
